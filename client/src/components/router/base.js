8ef3c5dcf4759803c70673e1d2e992612d601614
조준명 <a8456452@naver.com>
2020-02-24 22:31:25 +0900

    링크도 url 주소 변경 추가 디자인 시안 일부 적용

--- client/src/components/router/base.js
+++ client/src/components/router/base.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './base.css'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Main from './main/js/main';
 
 class Base extends React.Component {
     render() {
         return (
-            <div>
-                <div className="header">
-                    <span>a</span>
-                </div>
-                <div className="main">
-                    <Router>
+            <div className="base-div">
+                <Router>
+                    <div className="base-header">
+                        <div className="base-login">
+                            <Link to="/login" className="base-loginLink">로그인</Link>
+                            <Link to="/insert" className="base-loginLink">회원가입</Link>
+                            <Link to="/qanda" className="base-loginLink">고객센터</Link>
+                        </div>
+                        <div className="base-cate">
+                            <Link to="/" className="base-logo">INDIEGAME</Link>
+                            <Link to="/" className="base-tags base-tags-hover">HOME</Link>
+                            <Link to="/hot" className="base-tags">HOT</Link>
+                            <Link to="/new" className="base-tags">NEW</Link>
+                            <Link to="/pc" className="base-tags">PC</Link>
+                            <Link to="/mobile" className="base-tags">MOBILE</Link>
+                        </div>
+                    </div>
+                    <div className="base-main">
                         <Route exact path="/"> 
+                            <Main></Main>
                         </Route>
-                    </Router>
-                </div>
-                <div className="footer">
-                    <span>b</span>
-                </div>
+                    </div>
+                    <div className="base-footer">
+                        <span>footer</span>
+                    </div>
+                </Router>
             </div>
         );
     }
---
+++ client/src/components/router/main/js/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+    render() {
+        return (
+            <div>
+                main div
+            </div>
+        );
+    }
+}
+
+export default Main;
