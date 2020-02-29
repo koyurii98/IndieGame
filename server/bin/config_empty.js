373026d13e242b052122c9929e211f39b70ab5fa
조준명 <a8456452@naver.com>
2020-02-29 18:00:09 +0900

    데이터베이스 orm 추가 및 유저 테이블 추가 및 서버 실행시 데이터베이스 연결

--- server/bin/config_empty.js
+++ server/bin/config_empty.js
@@ -1,5 +1,13 @@
 let configVar = {
     port : "",
+
+    database : "",
+    user : "",
+    pass : "",
+    host : "",
+    dialect : "",
+
+    server_force : "",   // 테이블 서버 실행과 동시에 초기화 할 지 여부
 }
 
 module.exports = configVar;
---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("user", { // 테이블 이름 => users
+        userId : {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userPass: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userEmail: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userPhone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        userNum: {
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+    });
+    // 조인과 외래키 관련
+    //User.associate = function(models) {
+        //models.user.hasMany(models.group);
+        //models.user.belongsTo(models.group);
+    //};
+    return User;
+};
