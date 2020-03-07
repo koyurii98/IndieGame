01e7f52675121c577daa9ff9fda76a6181630603
조준명 <a8456452@naver.com>
2020-03-07 19:00:00 +0900

    유저 디비 기본 api 추가 -> 전체 조회, 하나 조회, 생성 , 수정, 삭제

--- client/src/components/router/insert/js/insert.js
+++ client/src/components/router/insert/js/insert.js
@@ -2,6 +2,23 @@ import React from 'react';
 import '../css/insert.css';
 
 class Insert extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            id : "",
+            pass : "",
+            passck : "",
+        }
+    }
+    clickInserBtn() {
+        console.log(this.inputDiv);
+    }
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value,
+        })
+        console.log(this.state)
+    }
     render () {
         return (
             <div className="insert-mainDiv">
@@ -15,46 +32,46 @@ class Insert extends React.Component {
                     <div className="insert-box">
                         <div className="insert-box-content">
                             <span>아이디</span>
-                            <input type="text"></input>
+                            <input name="id" onChange={this.handleInput.bind(this)} type="text"></input>
                         </div>
                         <div className="insert-box-content">
                             <span>비밀번호</span>
-                            <input type="text"></input>
+                            <input name="pass" onChange={this.handleInput.bind(this)} type="text"></input>
                         </div>
                         <div className="insert-box-content">
                             <span>비밀번호확인</span>
-                            <input type="text"></input>
+                            <input name="passck" onChange={this.handleInput.bind(this)} type="text"></input>
                         </div>
                         <div className="insert-box-content">
                             <span>이름</span>
-                            <input type="text"></input>
+                            <input name="name" onChange={this.handleInput.bind(this)} type="text"></input>
                         </div>
                         <div className="insert-box-content">
                             <span>이메일</span>
-                            <input type="text"></input>
+                            <input name="email" onChange={this.handleInput.bind(this)} type="text"></input>
                         </div>
                         <div className="insert-box-content">
                             <span>주민번호</span><br/>
-                            <input type="text"className="insert-input-usernumber"></input>
+                            <input name="num_1" onChange={this.handleInput.bind(this)} type="text"className="insert-input-usernumber"></input>
                             <span className="hipen"> - </span>
-                            <input type="text"className="insert-input-usernumber"></input>
+                            <input name="num_2" onChange={this.handleInput.bind(this)} type="text"className="insert-input-usernumber"></input>
                         </div>
                         <div className="insert-box-content">
                             <span>휴대폰번호</span>
-                            <input type="text"></input>
+                            <input name="phone" onChange={this.handleInput.bind(this)} type="text"></input>
                         </div>
                         <div className="insert-box-content">
                             <input type="text"className="insert-input-phoneck"></input>
                             <button className="insert-phonech-btn">인증번호받기</button>
                         </div>
                         <div className="insert-box-content">
-                            <button className="insert-btn">가입하기</button>
+                            <button className="insert-btn" onClick={this.clickInserBtn.bind(this)}>가입하기</button>
                         </div>
                     </div>
                 </div>
             </div>
-    )
-}
+        )
+    }
 }
 
 export default Insert;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,83 @@
 var express = require('express');
 var router = express.Router();
+let models = require("../models");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+let User = models.user;
+
+router.get('/all', async(req,res,next) => {
+    try {
+        const result = await User.findAll();
+        res.send(result);
+    } catch(err) {
+        console.log("users all err : " + err)
+    }
+});
+
+router.get('/one', async(req,res,next) => {
+    try {
+        const result = await User.findOne({
+            where : {
+                id : req.query.id,
+            }
+        });
+        res.send(result);
+    } catch(err) {
+        console.log("users one err : " + err)
+    }
+});
+
+router.post('/insert', async(req,res,next) => {
+    let result = true;
+    try {
+        await User.findOrCreate({
+            where : {
+                userId : req.body.userId,
+            },
+            default : {
+                userId : req.body.userId,
+            }
+        })
+    } catch(err) {
+        console.log("users insert err : " + err)
+        result = false;
+    }
+    res.send(result);
+});
+
+router.put('/update', async(req,res,next) => {
+    let result = true;
+    try {
+        await User.update({
+            userId : req.body.userId,
+            userPass : req.body.userPass,
+            userEmail : req.body.userEmail,
+            userPhone : req.body.userPhone,
+            userNum : req.body.userNum,
+            },{
+            where : {
+                id : req.body.id,
+            },
+        });
+    } catch(err) {
+        console.log("users all err : " + err)
+        result = false;
+    }
+    res.send(result);
+});
+
+router.delete('/delete', async(req,res,next) => {
+    let result = true;
+    try {
+        await User.destroy({
+            where : {
+                id : req.query.id,
+            }
+        });
+    } catch(err) {
+        console.log("users all err : " + err)
+        result = false;
+    }
+    res.send(result);
 });
 
 module.exports = router;
